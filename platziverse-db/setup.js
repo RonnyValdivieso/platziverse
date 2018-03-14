9be947587515d01905e580f277e2f8736f91b69f
Ronny Valdivieso <ron_251@hotmail.com>
2018-03-14 08:21:27 -0500

    database setup

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,14 +1,31 @@
 'use strict'
 
 const debug = require('debug')('platziverse:db::setup')
-const db = require('./lib')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const db = require('./')
+
+const prompt = inquirer.createPromptModule()
 
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'ronny',
     password: process.env.DB_PASS || 'ronny',
     host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
     logging: s => debug(s),
     setup: true
   }
@@ -16,29 +33,32 @@ async function setup () {
   await db(config).catch(handleFatalError)
 
   console.log('Success!')
+  process.exit(0)
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
 
-module.exports = async function (config) {
-  const sequelize = setupDatabase(config)
-  const AgentModel = setupAgentModel(config)
-  const MetricModel = setupMetricModel(config)
+setup()
 
-  AgentModel.hasMany(MetricModel)
-  MetricModel.belongsTo(AgentModel)
+// module.exports = async function (config) {
+//   const sequelize = setupDatabase(config)
+//   const AgentModel = setupAgentModel(config)
+//   const MetricModel = setupMetricModel(config)
 
-  await sequelize.authenticate()
+//   AgentModel.hasMany(MetricModel)
+//   MetricModel.belongsTo(AgentModel)
 
-  const Agent = {}
-  const Metric = {}
+//   await sequelize.authenticate()
 
-  return {
-    Agent,
-    Metric
-  }
-}
+//   const Agent = {}
+//   const Metric = {}
+
+//   return {
+//     Agent,
+//     Metric
+//   }
+// }
